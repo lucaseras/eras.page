b05d0c13be0d4d4a5f670c121f5e6624dce44395
Lucas Eras Paiva <lucaseraspaiva@gmail.com>
2021-05-11 21:33:03 -0400

    changed index

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,15 @@ import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -26,17 +35,7 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
-
-
     </Layout>
     )
 }
 
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
-    return {
-        props: {
-            allPostsData
-        }
-    }
-}
