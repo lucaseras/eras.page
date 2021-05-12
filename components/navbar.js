fe1aeb833a9c45a1da605535a3ee2b2e7605a08c
Lucas Eras Paiva <lucaseraspaiva@gmail.com>
2021-05-12 15:06:04 -0400

    created navbar component and style css

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+// React component that defines the navigation bar
+import styles from '../styles/navbar.css'
+
+export default function Navbar() {
+
+    return (
+        <nav className={styles.nav}>
+            <un className={styles.un}>
+        </nav>
+    )
+}
