09a015545b815ca68c128b98a58eb310a75c13bf
Lucas Eras Paiva <lucaseraspaiva@gmail.com>
2021-05-11 23:39:55 -0400

    fixed index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData()
@@ -13,29 +15,34 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-          <p>Hello! My name is Lucas, this is an introduction.</p>
-      </section>
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+    const postsAsList =
+        (allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
+                <Link href={`/posts/${id}`}>
+                    <a>{title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                    <Date dateString={date} />
+                </small>
             </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+        )))
+
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>Hello! My name is Lucas, this is an introduction.</p>
+            </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {postsAsList}
+                </ul>
+            </section>
+        </Layout>
     )
 }
 
