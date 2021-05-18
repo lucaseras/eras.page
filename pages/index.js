5b593b3281c7befff3facb42528fbd7cd5d8a84e
Lucas Eras Paiva <lucaseraspaiva@gmail.com>
2021-05-17 23:39:09 -0400

    config + new index page

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,19 @@
 import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import About from '../components/about'
 
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
-    return {
-        props: {
-            allPostsData
-        }
-    }
-}
 
-export default function Home({ allPostsData }) {
-    const postsAsList =
-        (allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-                <Link href={`/posts/${id}`}>
-                    <a>{title}</a>
-                </Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                    <Date dateString={date} />
-                </small>
-            </li>
-        )))
-
+export default function Home() {
     return (
         <Layout home>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>Website under construction. Feel free to look around!</p>
-            </section>
-            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                <h2 className={utilStyles.headingLg}>Blog</h2>
-                <ul className={utilStyles.list}>
-                    {postsAsList}
-                </ul>
+            <About></About>
             </section>
         </Layout>
     )
