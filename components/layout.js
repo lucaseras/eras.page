b0434c0080f808760370c264072d9ad2e08543b6
Lucas Eras Paiva <lucaseraspaiva@gmail.com>
2021-05-19 13:10:43 -0400

    fixed back button positioning

--- components/layout.js
+++ components/layout.js
@@ -10,28 +10,28 @@ export const siteTitle = 'eras.page'
 export default function Layout({ children, home, post}) {
     return (
         <div className={styles.mainContainer}>
-        <Head>
-            <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
-        </Head>
-        <Navbar></Navbar>
-        <div className={styles.container}>
-            {(!home && !post) && (
-                <div className={styles.backToHome}>
-                    <Link href="/">
-                        <a>← Back to home</a>
-                    </Link>
-                </div>
+            <Head>
+                <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+            </Head>
+            <Navbar></Navbar>
+            <div className={styles.container}>
+                {(!home && !post) && (
+                    <div className={styles.back}>
+                        <Link href="/">
+                            <a>← Back to home</a>
+                        </Link>
+                    </div>
                 )}
                 {post && (
-                    <div className={styles.backToHome}>
+                    <div className={styles.back}>
                         <Link href='/blog'>
                             <a>← Back to blog</a>
                         </Link>
                     </div>
-            )}
+                )}
                 <main>{children}</main>
             </div>
             <Footer></Footer>
         </div>
-)
+    )
 } 
