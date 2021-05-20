57b8d15dc833aa35849fe9861c451533f7122d83
Lucas Eras Paiva <lucaseraspaiva@gmail.com>
2021-05-20 00:23:56 -0400

    removed dynamic navbar background :( now it is 100% css

--- components/navbar.js
+++ components/navbar.js
@@ -16,31 +16,9 @@ const items = [
 ]
 
 export default function Navbar() {
-    const [backgroundOpacity, setBackgroundOpacity] = useState(0)
-
-    const handleScroll = () => {
-        if (window.pageYOffset == 0) {
-            setBackgroundOpacity(0)
-        }
-        else if (window.pageYOffset < 60) {
-            setBackgroundOpacity(window.pageYOffset / 65)
-        } else {
-            setBackgroundOpacity(1)
-        }
-    }
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        };
-    });
-    const navbarStyle = {
-        background: `linear-gradient(to bottom, rgba(255,255,255, ${backgroundOpacity}) 85%, rgba(255, 255, 255, 0))`
-        //backgroundColor: `rgba(240,240,240, ${backgroundOpacity})`
-    }
 
     return (
-        <nav className={styles.navbar} style={navbarStyle}>
+        <nav className={styles.navbar}>
             <Link href='/'>
                 <h1 className={styles.h1}>
                     <a className={styles.a}>eras.page</a>
