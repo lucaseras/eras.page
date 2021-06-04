fa4b2e125ec513280284baad3bbde8e23bb10e79
Lucas Eras Paiva <lucaseraspaiva@gmail.com>
2021-06-04 12:03:48 -0700

    added names to footer links

--- components/footer.js
+++ components/footer.js
@@ -2,59 +2,65 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/footer.module.css'
 import useWindowDimensions from './useWindowDimensions'
-import { useTheme } from '../styles/theme'
-
-const githubLogo = (
-    <Link
-        href='https://github.com/lucaseras'
-    >
-        <a>
-            <Image
-                src='/github.png'
-                alt={'Github account'}
-                width={32}
-                height={32}
-            />
-        </a>
-    </Link>
-)
-
-const linkedinLogo = (
-    <Link
-        href='https://www.linkedin.com/in/lucaseraspaiva/'
-    >
-        <a>
-            <Image
-                src='/linkedin.png'
-                alt={'Linkedin account'}
-                width={34}
-                height={34}
-            />
-        </a>
-    </Link>
-)
-
-const madeByText = (width) => {
-    if (width > 500) {
-        return "Made by Lucas Eras using React.js and Next.js"
-    } else {
-        return "Made by Lucas Eras"
-    }
-}
+import {useTheme} from '../styles/theme'
 
 export default function bottom() {
     const [isDark, , ] = useTheme()
     const {width} = useWindowDimensions();
 
+    const githubLogo = (
+        <div>
+        <Link
+            href='https://github.com/lucaseras'
+        >
+            <a>
+                <Image
+                    src='/github.png'
+                    alt={'Github account'}
+                    width={34}
+                        height={34}
+                    />
+                <p style={{marginTop: '0px'}}>GitHub</p>
+            </a>
+        </Link>
+        </div>
+    )
+
+    const linkedinLogo = (
+        <div>
+        <Link
+            href='https://www.linkedin.com/in/lucaseraspaiva/'
+        >
+            <a>
+                <Image
+                    src='/linkedin.png'
+                    alt={'Linkedin account'}
+                    width={34}
+                    height={34}
+                />
+                <p style={{marginTop: '0px'}}>LinkedIn</p>
+            </a>
+        </Link>
+        </div>
+    )
+
+    const madeByText = (width) => {
+        if (width > 500) {
+            return "Made by Lucas Eras using React.js and Next.js"
+        } else {
+            return "Made by Lucas Eras"
+        }
+    }
+
     return (
-            <div className={styles.main}>
-                <p className={styles.text}>
+        <div className={styles.main}>
+            <p className={styles.text}>
                 {madeByText(width)}
             </p>
-                <div className={`${styles.logos} ${isDark ? styles.inverted : ''}`}>
-                    {githubLogo}
-                    {linkedinLogo}
-                </div>
+            <div className={`${styles.logos} ${isDark ? styles.inverted : ''}`}>
+                {githubLogo}
+                {linkedinLogo}
             </div>
+        </div>
     )
 }
